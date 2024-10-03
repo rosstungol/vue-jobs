6265d0d4a507f55c878e2139db7ae0a682819e93
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-04 07:41:44 +0800

    Implements JobListings loading

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,7 @@ import { ref, defineProps, onMounted, reactive } from "vue";
 import { RouterLink } from "vue-router";
 import axios from "axios";
 import JobListing from "./JobListing.vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 const state = reactive({
   jobs: [],
@@ -35,7 +36,11 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in state.jobs.slice(0, limit) || state.jobs.length"
           :key="job.id"
