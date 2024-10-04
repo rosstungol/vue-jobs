0139bc0d5e086ca2ed2a207e630926697bc5842f
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-04 08:16:09 +0800

    Implements dynamic Job view

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -40,6 +40,7 @@ onMounted(async () => {
       <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
         <PulseLoader />
       </div>
+
       <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in state.jobs.slice(0, limit) || state.jobs.length"
