fbed799947d49ff5ea774d98f7b8ab53fcbdad72
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-04 12:38:05 +0800

    Adds window.confirm to job delete feature

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -19,9 +19,13 @@ const state = reactive({
 
 const deleteJob = async () => {
   try {
-    await axios.delete(`/api/jobs/${jobId}`);
-    toast.success("Job deleted successfully");
-    router.push("/jobs");
+    const confirm = window.confirm("Are you sure you want to delete this job?");
+
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job deleted successfully");
+      router.push("/jobs");
+    }
   } catch (error) {
     console.error("Error deleting job", error);
     toast("Job not deleted");
